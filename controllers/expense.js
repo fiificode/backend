178e88be208606d73aa54cffa243515dc37c8cc1
Franklin Mensah <fiificode@gmail.com>
2023-04-12 22:14:59 +0000

    creating expenses model

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,10 +1,10 @@
-const income = require("../models/income")
-const IncomeSchema = require("../models/income")
+const expense = require("../models/income")
+const ExpenseSchema = require("../models/expense")
 
-exports.addIncome = async (req,res) => {
+exports.addExpense = async (req,res) => {
     const {title,amount,category,description,date} = req.body
 
-    const income = IncomeSchema({
+    const expense = ExpenseSchema({
         title,
         amount,
         category,
@@ -19,29 +19,29 @@ exports.addIncome = async (req,res) => {
         if(amount <= 0 || !amount === "number"){
             return res.status(400).json({message: "Amount should be a number!"})
         }
-        await income.save()
-        res.status(200).json({message:"Income added successfully!"})
+        await expense.save()
+        res.status(200).json({message:"Expense added successfully!"})
     } catch (error) {
         res.status(500).json({message:"Server Error!"})
         
     }
 }
 
-exports.getIncome = async (req,res) => {
+exports.getExpenses = async (req,res) => {
     try {
-        const incomes = await IncomeSchema.find().sort({createdAt:-1})
-        res.status(200).json(incomes)
+        const expenses = await ExpenseSchema.find().sort({createdAt:-1})
+        res.status(200).json(expenses)
     } catch (error) {
         res.status(500).json({message:"Server Error"})
     }
 }
 
 
-exports.deleteIncome = async (req,res) => {
+exports.deleteExpense = async (req,res) => {
     const {id } = req.params;
-    IncomeSchema.findByIdAndDelete(id)
-        .then((income) => {
-            res.status(200).json({message:"Income deleted!"})
+    ExpenseSchema.findByIdAndDelete(id)
+        .then((expense) => {
+            res.status(200).json({message:"Expense deleted!"})
         })
         .catch((error) => {
             res.status(500).json({message: "Server Error"})
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,9 +1,19 @@
-const { addIncome } = require('../controllers/income')
+const { addExpense, getExpenses, deleteExpense } = require('../controllers/expense')
+const { addIncome, getIncome, deleteIncome } = require('../controllers/income')
 
 const router = require('express').Router()
 
 
 
 router.post('/add-income', addIncome)
+        .get('/get-incomes',getIncome)
+        .delete('/delete-income/:id',deleteIncome)
 
+        .post('/add-expense',addExpense)
+        .get('/get-expenses',getExpenses)
+        .delete('/delete-expense/:id',deleteExpense)
+
+
+
+        
 module.exports = router
